5a8b1a4bba2068cf85164a4fdc6ccd98af5a3551
lukaszchelkowski <79515048+lukaszchelkowski@users.noreply.github.com>
2021-02-25 13:27:41 +0100

    Zadania 25.02

---
+++ src/First_week/basics/Zadania.java
@@ -0,0 +1,34 @@
+package First_week.basics;
+
+public class Zadania {
+    public static void main(String[] args) {
+        //Zadanie 1
+     int myIntA = 18;
+     double myDoubleB = 4.5;
+     String myStringC ="Jestem Lukasz mam lat ";
+     char myCharD = '$';
+        System.out.println(myStringC + myIntA);
+        System.out.println("Moja srednia z zeszlego roku to: "+ myDoubleB);
+        System.out.println("Moj ulubiony znak to :" + myCharD);
+
+        //zadanie 2
+     int A =5;
+     int B =7;
+     int sum= (A*A) + (B*B);
+        System.out.println("Suma = " + sum);
+
+        //Zadanie 3
+     double sqrt = Math.sqrt(3);
+     int a = 7;
+     double wynik = a*a * sqrt/4;
+        System.out.println("Pole równe jest " + wynik);
+
+        //Zadanie 4
+     String myStringImie= "Lukasz";
+     String myStringNazwisko= "Chełkowski";
+        System.out.println(myStringImie + myStringNazwisko);
+
+
+
+    }
+}
